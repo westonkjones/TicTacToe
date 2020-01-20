27d0c5fbe74be9c56cca68ec424be7bbc7f0b88f
Weston Jones <westonkjones@gmail.com>
2020-01-20 13:43:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let winCombos = [
     [cells[0], cells[4], cells[8]]
 ]
 
-cells.forEach(function (cell) {
-    cell.addEventListener("click", function () {
+cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
         if (turnCounter % 2 === 0) {
             cell.textContent = "X";
         } else {
@@ -24,44 +24,32 @@ cells.forEach(function (cell) {
     });
 });
 
+function check(mark) {
+    console.log("Checking for " + mark);
+    winCombos.forEach((combo) => {
+        let sum = 0;
+        combo.forEach((c) => {
+            if (c.textContent === mark) {
+                sum++
+            }
+
+            if (sum === 3) {
+                alert(mark + " Wins!")
+                reset()
+                return;
+            }
+        });
+    });
+}
+
 function checkWin() {
-    // length of 8
-    for (let i = 0; i < winCombos.length; i++) {
-        let sum = 0;
-        // length of 2
-        for (let j = 0; j < winCombos[i].length; j++) {
-            if (winCombos[i][j].textContent === "X") {
-                sum++
-            }
-
-            if (sum === 3) {
-                alert("X Wins!")
-                reset()
-                return;
-            }
-        }
-    }
-
-    for (let l = 0; l < winCombos.length; l++) {
-        let sum = 0;
-        for (let k = 0; j < winCombos[l].length; k++) {
-            if (winCombos[l][k].textContent === "O") {
-                sum++
-            }
-
-            if (sum === 3) {
-                alert("O Wins!")
-                reset()
-                return;
-            }
-        }
-    }
+    check("X");
+    check("O");
 }
 
 function reset() {
-
-    for (i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = ""
+    cells.forEach((cell) => {
+       cell.innerHTML = "";
         turnCounter = 0;
-    }
+    });
 }
